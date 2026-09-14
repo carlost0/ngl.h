65629e717a7f77965365dd434db93972d39b193b
Carlos GS <carlosgs161@proton.me>
2026-09-14 16:36:47 +0200

    Changed font system to use Hex literals instead of Binary literals for true C99 support.

--- extra/nglfontbuilder.c
+++ extra/nglfontbuilder.c
@@ -11,57 +11,107 @@ int main() {
     int w = 5, h = 5;
     int line_thickness = 3;
 
-    int *cells = (int *) malloc(w*h * sizeof(int));
-    memset(cells, 0, w*h*sizeof(int));
+    char *cells = (char*)malloc(w*h * sizeof(char));
+    memset(cells, '0', w*h * sizeof(char));
     InitWindow(w * RATIO_W, h * RATIO_H, "fontbuilder");
 
     printf("Press P to print the Sprite as a C String.\n");
-    printf("Press C or Mouse 3 to Clear the Screen as a C String.\n");
+    printf("Press C or Mouse 3 to Clear the Screen.\n");
+
     while (!WindowShouldClose()) {
         BeginDrawing();
 
+        ClearBackground(BLACK);
+
         for (int y = 0; y < h; ++y) {
             for (int x = 0; x < w; ++x) {
-                if (cells[y * w + x]) {
-                    DrawRectangle(x * RATIO_W, y * RATIO_H, RATIO_W, RATIO_H, RAYWHITE);
-                } else {
-                    DrawRectangle(x * RATIO_W, y * RATIO_H, RATIO_W, RATIO_H, BLACK);
-                }
-            }       
-        }       
+                Color color = cells[y * w + x] == '1'
+                            ? RAYWHITE
+                            : BLACK;
 
-        for (int x = 0; x < w; ++x)
-            DrawRectangle(x * RATIO_W, 0, line_thickness, h * RATIO_H, GRAY);
-        for (int y = 0; y < h; ++y)
-            DrawRectangle(0, y * RATIO_H, w * RATIO_W, line_thickness, GRAY);
+                DrawRectangle(
+                    x * RATIO_W,
+                    y * RATIO_H,
+                    RATIO_W,
+                    RATIO_H,
+                    color
+                );
+            }
+        }
+
+        for (int x = 0; x <= w; ++x) {
+            DrawRectangle(
+                x * RATIO_W,
+                0,
+                line_thickness,
+                h * RATIO_H,
+                GRAY
+            );
+        }
+
+        for (int y = 0; y <= h; ++y) {
+            DrawRectangle(
+                0,
+                y * RATIO_H,
+                w * RATIO_W,
+                line_thickness,
+                GRAY
+            );
+        }
 
         EndDrawing();
 
         int mx = GetMouseX();
         int my = GetMouseY();
 
-        int mouse_i = (my/RATIO_H) * w + (mx/RATIO_W);
+        int cell_x = mx / RATIO_W;
+        int cell_y = my / RATIO_H;
 
-        if (IsMouseButtonDown(0)) {
-            cells[((int)(my/RATIO_H)) * w + ((int)(mx/RATIO_W))] = 1;
-        } else if (IsMouseButtonDown(1)) {
-            cells[((int)(my/RATIO_H)) * w + ((int)(mx/RATIO_W))] = 0;
-        } else if (IsMouseButtonDown(2) || IsKeyPressed(KEY_C)) {
-            memset(cells, 0, w*h*sizeof(int));
+        if (cell_x >= 0 && cell_x < w &&
+            cell_y >= 0 && cell_y < h) {
+
+            int index = cell_y * w + cell_x;
+
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                cells[index] = '1';
+            } else if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+                cells[index] = '0';
+            }
+        }
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE) ||
+            IsKeyPressed(KEY_C)) {
+            memset(cells, '0', w * h);
         }
 
         if (IsKeyPressed(KEY_P)) {
-            printf("NGL_GLYPH(");
-            for (int y = 0; y < h; ++y) {
-                printf("0b");
-                for (int x = w-1; x >= 0; --x) {
-                    printf("%d", cells[x + y * w]);
+        printf("NGL_GLYPH(");
+
+        for (int y = 0; y < h; ++y) {
+            unsigned int value = 0;
+
+            // Read each row from right to left
+            for (int x = w - 1; x >= 0; --x) {
+                value <<= 1;
+
+                if (cells[y * w + x] == '1') {
+                    value |= 1;
                 }
-                if (y != h-1) printf(",");
             }
+
+            printf("0x%02x", value);
+
+            if (y != h - 1) {
+                printf(",");
+            }
+        }
+
             printf("),\n");
         }
+
     }
 
+
+    free(cells);
     CloseWindow();
 }
